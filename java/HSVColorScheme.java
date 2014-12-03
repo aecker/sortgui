026793936ed712ef25f61f5698ea16946af8f2ab
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-12-03 16:01:59 +0100

    Updated HSV color scheme for better visibility

--- java/HSVColorScheme.java
+++ java/HSVColorScheme.java
@@ -2,8 +2,8 @@
 public class HSVColorScheme implements ColorScheme {
 
 	private static float[][] colors = {
-		{1, 0, 0}, {0.2f, 1, 0}, {0, 0.4f, 1}, {1, 0, 0.6f}, {0.8f, 1, 0},  
-		{0, 1, 1}, {0.8f, 0, 1}, {1, 0.6f, 0}, {0, 1, 0.4f}, {0.2f, 0, 1}
+		{1, 0, 0}, {0.8f, 1, 0}, {0, 0.8f, 1}, {0.8f, 0, 1}, {1, 0.8f, 0},  
+		{0, 1, 0.8f}, {0.8f, 0.8f, 0.8f}, {1, 0.4f, 0}, {0.4f, 1, 0}, {0, 0.4f, 1}
 	};
 	
 	@Override
