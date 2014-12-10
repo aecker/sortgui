cbb7288f4b6120d7c1917f865ceee4fcfba1dc25
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-12-10 15:19:59 +0100

    Updated cluster list using table

--- matlab/SortGUI.m
+++ matlab/SortGUI.m
@@ -2,14 +2,22 @@ classdef SortGUI < handle
     
     properties
         fig
+        table
         selection
         up
         down
         more
         less
+        colorScheme
         ccgView
         spikeView
         waveView
+        su
+        ignore
+        rate
+        selFirst
+        selLast
+        M
     end
     
     properties (Access = private, Constant)
@@ -23,7 +31,7 @@ classdef SortGUI < handle
             
             % GUI main window
             height = 400;
-            width = 130;
+            width = 270;
             pos = getenv('SORTGUI_POSITION');
             if isempty(pos)
                 left = 50;
@@ -36,16 +44,22 @@ classdef SortGUI < handle
             pos = [left, top - height, width, height];
             self.fig = figure('MenuBar', 'none', 'ToolBar', 'none', ...
                 'NumberTitle', 'off', 'Name', 'Selection', 'Position', pos);
+            self.colorScheme = HSVColorScheme;
             
             % cluster list
-            M = max(assignments);
+            self.M = max(assignments);
             s = self.spacing;
             h = height - 2 * s;
-            w = 50;
+            w = 200;
             pos = [s, height - h - s, w, h];
-            v = arrayfun(@num2str, 1 : M, 'uni', false);
-            self.selection = uicontrol('Style', 'list', 'Position', pos, ...
-                'Min', 0, 'Max', Inf, 'FontSize', 12, 'String', v);
+            
+            self.table = uitable('Data', {}, 'RowName', [], ...
+                'ColumnName', {'Color', 'SU', 'Rate', 'Ignore'}, ...
+                'ColumnWidth', {40 20 40 40}, ...
+                'ColumnEditable', [false, true, false, true], ...
+                'BackgroundColor', [1 1 1], ...
+                'Position', pos, 'FontSize', 12, ...
+                'CellEditCallback', @(src, evt) self.tableEdit(src, evt));
             
             % buttons for moving the list
             h = 30;
@@ -66,10 +80,11 @@ classdef SortGUI < handle
                 'FontSize', 14, 'String', '+', 'Callback', @(~, ~) self.expandSel(1));
             
             % cross-correlograms
-            ccg = correlogram(t, assignments, M, 0.2, 10);
+            ccg = correlogram(t, assignments, self.M, 0.2, 10);
             self.ccgView = MCorrelogramView;
             self.ccgView.setCCG(ccg)
-            w = 600;
+            w = 1200;
+            height = 800;
             set(self.ccgView, 'Position', [left + width, top - height, w, height]);
             
             % waveforms
@@ -77,26 +92,25 @@ classdef SortGUI < handle
             self.waveView = MWaveformView;
             self.waveView.setChannelLayout(channelLayout(:, 1), channelLayout(:, 2))
             self.waveView.setWaveforms(W)
-            set(self.waveView, 'Position', [left + width, top - 2 * height - th, w, height]);
+            set(self.waveView, 'Position', [left + width, top - 1.5 * height - th, w, 0.5 * height]);
             
             % spike times
             self.spikeView = MSpikeTimeView;
             self.spikeView.setSpikes(t, ampl, assignments)
-            set(self.spikeView, 'Position', [left + width + w, top - height, w, height]);
+            set(self.spikeView, 'Position', [left + width + w, top - height, round(w / 3), height]);
             
-            % set selection
-            sel = 1 : min(8, M);
-            self.selection.Callback = @(src, ~) self.setSelected(src.Value);
-            addlistener(self.selection, 'Value', 'PostSet', ...
-                @(~, evt) self.setSelected(evt.AffectedObject.Value));
-            self.selection.Value = sel;
+            % statistics etc
+            self.rate = histc(assignments, 1 : self.M) / (t(end) - t(1)) * 1000;
+            self.su = false(self.M, 1);
+            self.ignore = false(self.M, 1);
+            self.setSelection(1, 15);
             
-            self.fig.KeyPressFcn = @(~, evt) self.handleKeyboard(evt);
-            self.up.KeyPressFcn = @(~, evt) self.handleKeyboard(evt);
-            self.down.KeyPressFcn = @(~, evt) self.handleKeyboard(evt);
-            self.less.KeyPressFcn = @(~, evt) self.handleKeyboard(evt);
-            self.more.KeyPressFcn = @(~, evt) self.handleKeyboard(evt);
-            self.selection.KeyPressFcn = @(~, evt) self.handleKeyboard(evt);
+            set(self.fig, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
+            set(self.up, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
+            set(self.down, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
+            set(self.less, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
+            set(self.more, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
+            set(self.table, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
             set(self.ccgView, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
             set(self.waveView, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
             set(self.spikeView, 'KeyPressFcn', @(~, evt) self.handleKeyboard(evt));
@@ -108,35 +122,52 @@ classdef SortGUI < handle
     methods (Access = private)
         
         function moveSel(self, i)
-            sel = self.selection.Value;
-            s = min(sel);
-            e = max(sel);
-            if s + i < 1 || e + i > numel(self.selection.String)
-                return
-            end
-            ignore = setdiff(s : e, sel);
-            self.selection.Value = setdiff(s + i : e + i, ignore);
+            self.setSelection(self.selFirst + i, self.selLast + i);
         end
         
         function expandSel(self, i)
-            sel = self.selection.Value;
-            s = min(sel);
-            e = max(sel);
-            ignore = setdiff(s : e, sel);
-            if e + i > numel(self.selection.String)
-                if s - i < 1
-                    return
-                end
-                self.selection.Value = setdiff(s - i : e, ignore);
-            else
-                self.selection.Value = setdiff(s : e + i, ignore);
-            end
+            self.setSelection(self.selFirst, self.selLast + i);
         end
         
-        function setSelected(self, sel)
+        function setSelection(self, first, last)
+            
+            % determine units to be selected
+            if last > self.M
+                first = first - last + self.M;
+                last = self.M;
+            end
+            if first < 1
+                last = last + 1 - first;
+                first = 1;
+            end
+            self.selFirst = min(self.M, max(1, first));
+            self.selLast = min(self.M, max(1, last));
+            indices = (self.selFirst : self.selLast)';
+            
+            % update stats table
+            colors = arrayfun(@(k) colorPatch(self.colorScheme.getColor(k - 1)), indices, 'uni', false);
+            [colors{self.ignore(indices)}] = deal('');
+            self.table.RowName = num2cell(indices);
+            self.table.Data = [colors, num2cell(self.su(indices)), ...
+                arrayfun(@(x) sprintf('<html><p align="right" width=35">%.1f</p></html>', x), self.rate(indices), 'uni', false), ...
+                num2cell(self.ignore(indices))];
+            
+            % update views
+            sel = indices(~self.ignore(indices));
             self.ccgView.setSelected(sel);
             self.spikeView.setSelected(sel);
             self.waveView.setSelected(sel);
+        end
+        
+        function tableEdit(self, ~, evt)
+            indices = self.selFirst : self.selLast;
+            switch evt.Indices(2)
+                case 2
+                    self.su(indices(evt.Indices(1))) = evt.NewData;
+                case 4
+                    self.ignore(indices(evt.Indices(1))) = evt.NewData;
+            end
+            self.setSelection(self.selFirst, self.selLast);
         end
         
         function handleKeyboard(self, evt)
@@ -149,6 +180,10 @@ classdef SortGUI < handle
                     self.moveSel(-1);
                 case 'rightarrow'
                     self.moveSel(1);
+                case {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+                    k = str2double(evt.Key);
+                    k = k + 10 * (k == 0);
+                    self.setSelection(self.selFirst, self.selFirst + k - 1);
                 case 'h'
                     disp ' '
                     disp 'Key bindings'
@@ -158,9 +193,18 @@ classdef SortGUI < handle
                     disp ' left  | move selection backward'
                     disp ' right | move selection forward'
                     disp ' h     | help'
+                    disp ' 0-9   | select N clusters (0: select 10)'
                     disp ' '
             end
         end
         
     end
 end
+
+
+function html = colorPatch(color)
+    color = round(color * 255);
+    html = sprintf('<html><div style="background: #%s%s%s; width: 25px; height: 8px"></div></html>', ...
+        dec2hex(color(1), 2), dec2hex(color(2), 2), dec2hex(color(3), 2));
+end
+
