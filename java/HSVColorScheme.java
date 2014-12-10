f0ffcbdc6ecf6f52a31c789393f7c667e3001c03
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-12-10 15:18:41 +0100

    Improved colors

--- java/HSVColorScheme.java
+++ java/HSVColorScheme.java
@@ -2,8 +2,8 @@
 public class HSVColorScheme implements ColorScheme {
 
 	private static float[][] colors = {
-		{1, 0, 0}, {0.8f, 1, 0}, {0, 0.8f, 1}, {0.8f, 0, 1}, {1, 0.8f, 0},  
-		{0, 1, 0.8f}, {0.8f, 0.8f, 0.8f}, {1, 0.4f, 0}, {0.4f, 1, 0}, {0, 0.4f, 1}
+		{1, 0, 0}, {0.8f, 1, 0}, {0, 0.8f, 1}, {1, 0, 0.6f}, {1, 0.8f, 0},  
+		{0, 1, 0.8f}, {0.7f, 0, 1}, {1, 0.4f, 0}, {0.4f, 1, 0}, {0, 0.4f, 1}
 	};
 	
 	@Override
