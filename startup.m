07d4e25f0d67a2170de12fdec03dd1384141ad5c
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-12-19 17:24:57 +0100

    Moved Java path to static class path

--- startup.m
+++ startup.m
@@ -6,5 +6,5 @@ addpath(fullfile(p, 'matlab'))
 addpath(fullfile(p, 'matlab/util'))
 addpath(fullfile(p, 'matlab/util/uicomponent'))
 
-% Java path
-javaaddpath(fullfile(p, 'bin'))
+% Java path (needs to be on static class path: edit classpath.txt)
+% javaaddpath(fullfile(p, 'bin'))
